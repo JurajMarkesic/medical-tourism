bdaaece90d609c3e050cb50fef47345b391f64a0
Marko Ronik <13383578+ronca85@users.noreply.github.com>
2020-05-07 21:52:10 +0200

    added new scss setup

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -29,7 +29,8 @@ module.exports = {
   /*
    ** Global CSS
    */
-  css: ['~assets/scss/main.scss'],
+  // css: ['~assets/scss/main.scss'],
+  css: ['~assets/styles/main.scss'],
   /*
    ** Plugins to load before mounting the App
    */
