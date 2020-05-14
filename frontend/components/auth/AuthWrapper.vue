acb2de75c65790f6221c21bd6fc7193f230526b9
Juraj Markesic <juraj.markesic.dev@gmail.com>
2020-05-14 18:03:23 +0200

    Added transition while changing auth components.

--- frontend/components/auth/AuthWrapper.vue
+++ frontend/components/auth/AuthWrapper.vue
@@ -1,8 +1,10 @@
 <template>
   <div>
-    <Register v-if="shownComponent === 'Register'"></Register>
-    <ForgottenPasword v-else-if="shownComponent === 'Forgotten Password'"></ForgottenPasword>
-    <Login v-else></Login>
+    <transition name="slide-fade">
+      <Register v-if="shownComponent === 'Register'"></Register>
+      <ForgottenPasword v-else-if="shownComponent === 'Forgotten Password'"></ForgottenPasword>
+      <Login v-else></Login>
+    </transition>
   </div>
 </template>
 
@@ -22,4 +24,16 @@ export default {
 };
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.slide-fade-enter-active {
+  transition: all 0.3s ease;
+}
+.slide-fade-leave-active {
+  transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
+}
+.slide-fade-enter, .slide-fade-leave-to
+/* .slide-fade-leave-active below version 2.1.8 */ {
+  transform: translateX(10px);
+  opacity: 0;
+}
+</style>
