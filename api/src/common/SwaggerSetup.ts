834eaf1bf65787d3fc554ea7e84a866b7bf65e66
JurajMarkesic <jmarkesic@tvz.hr>
2020-05-25 18:04:44 +0200

    Added the strapi docker service and connected it with nginx and postgres services.

--- api/src/common/SwaggerSetup.ts
+++ api/src/common/SwaggerSetup.ts
@@ -2,7 +2,7 @@ import { INestApplication } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 export const setupSwagger = (app: INestApplication) => {
-  if (process.env.NODE_ENV == 'DEV') {
+  if (process.env.NODE_ENV == 'development') {
     const options = new DocumentBuilder().setTitle('API documentation').build();
     const document = SwaggerModule.createDocument(app, options);
     SwaggerModule.setup(process.env.APP_PREFIX, app, document);
