0156d19576907193561f5fc91753c274671ed8b3
Marko Ronik <13383578+ronca85@users.noreply.github.com>
2020-05-31 22:50:15 +0200

    added amenities to clinic single

--- frontend/components/clinics/single/treatments/TreatmentsListItem.vue
+++ frontend/components/clinics/single/treatments/TreatmentsListItem.vue
@@ -4,14 +4,14 @@
       <span class="o-table_row_treatment_name">{{ treatment.name }}</span>
       <span class="o-table_row_treatment_price">{{ treatment.price }} €</span>
     </div>
-    <a href="" class="o-table_row_treatment_button || o-button" type="button" aria-label="Enquire now">
-      <span class="o-button_label" @click.prevent="contact()">Enquire now</span>
+    <button class="o-table_row_treatment_button || o-button" aria-label="Enquire now">
+      <span class="o-button_label" @click="contact()">Enquire now</span>
       <span class="o-button_icon">
         <svg viewBox="0 0 20 16.8">
           <path d="M12,16.8l-1.4-1.4l6-6H0v-2h16.6l-6-6L12,0l7.7,7.7c0.4,0.4,0.4,1,0,1.4L12,16.8z" />
         </svg>
       </span>
-    </a>
+    </button>
   </div>
 </template>
 
--- frontend/pages/clinics/_id.vue
+++ frontend/pages/clinics/_id.vue
@@ -4,11 +4,19 @@
       <div class="o-container">
         <article class="c-clinic">
           <h1 class="o-h2">Poliklinika Bagatin</h1>
-          <div>
-            <span>Zagreb, Croatia</span>
-          </div>
-          <div>
-            <button class="o-button -padding" @click="showClinicContact()">CONTACT</button>
+          <span>Zagreb, Croatia</span>
+          <div class="c-clinic_contact">
+            <div class="c-clinic_contact_phone">
+              <span class="u-icon">phone</span><a href="tel:+385915551234">091 555 1234</a>
+            </div>
+            <button class="o-button" @click="showClinicContact()">
+              <span class="o-button_label">Open contact form</span>
+              <span class="o-button_icon">
+                <svg viewBox="0 0 20 16.8">
+                  <path d="M12,16.8l-1.4-1.4l6-6H0v-2h16.6l-6-6L12,0l7.7,7.7c0.4,0.4,0.4,1,0,1.4L12,16.8z" />
+                </svg>
+              </span>
+            </button>
           </div>
 
           <ClinicContactModal />
@@ -17,37 +25,62 @@
 
           <ClinicSingleTabContent />
 
-          <h3>ADRESA</h3>
-          <h1>KARTA SA LOKACIJOM</h1>
+          <div class="o-layout_item u-1/2@from-small u-2/5@from-large">
+            <pre>KARTA SA LOKACIJOM</pre>
 
-          <div id="goto-amenities" class="o-layout -gutter">
-            <div class="o-layout_item || u-1/2@from-small u-2/5@from-large">
-              amenity a
-            </div>
-            <div class="o-layout_item || u-1/2@from-small u-2/5@from-large">
-              amenity b
-            </div>
-            <div class="o-layout_item || u-1/2@from-small u-2/5@from-large">
-              amenity d
-            </div>
-            <div class="o-layout_item u-1/2@from-small u-3/5@from-large">
-              amenity d,e,f,g,sdglfd,fsdfsd
-            </div>
+            <address class="c-clinic_address">
+              Bagatin<br />
+              Grada Vukovara 269a/10<br />
+              10000 Zagreb
+            </address>
+          </div>
 
-            <div class="o-layout_item u-1/2@from-small u-3/5@from-large">
-              Opening hours Monday08:30 - 16:00 Tuesday08:30 - 16:00 Wednesday08:30 - 16:00 Thursday08:30 - 16:00
-              Friday08:30 - 16:00 Saturday09:00 - 12:00 Sunday - closed
+          <div id="goto-amenities" class="c-amenities_list">
+            <div class="c-amenity">
+              <span class="c-amenity_icon">
+                <svg viewBox="0 0 20 16.8">
+                  <path d="M12,16.8l-1.4-1.4l6-6H0v-2h16.6l-6-6L12,0l7.7,7.7c0.4,0.4,0.4,1,0,1.4L12,16.8z" />
+                </svg>
+              </span>
+              <span class="c-amenity_name">Airport Pick-Up</span>
             </div>
-
-            <div class="o-layout_item u-1/2@from-small u-3/5@from-large">
-              <ul>
-                Payment methods
-                <li>Credit cards</li>
-                <li>Up to 12 rates</li>
-                <li>CAsh discount</li>
-              </ul>
+            <div class="c-amenity">
+              <span class="c-amenity_icon">
+                <svg viewBox="0 0 20 16.8">
+                  <path d="M12,16.8l-1.4-1.4l6-6H0v-2h16.6l-6-6L12,0l7.7,7.7c0.4,0.4,0.4,1,0,1.4L12,16.8z" />
+                </svg>
+              </span>
+              <span class="c-amenity_name">Free Wifi</span>
+            </div>
+            <div class="c-amenity">
+              <span class="c-amenity_icon">
+                <svg viewBox="0 0 20 16.8">
+                  <path d="M12,16.8l-1.4-1.4l6-6H0v-2h16.6l-6-6L12,0l7.7,7.7c0.4,0.4,0.4,1,0,1.4L12,16.8z" />
+                </svg>
+              </span>
+              <span class="c-amenity_name">Hotel Booking</span>
+            </div>
+            <div class="c-amenity">
+              <span class="c-amenity_icon">
+                <svg viewBox="0 0 20 16.8">
+                  <path d="M12,16.8l-1.4-1.4l6-6H0v-2h16.6l-6-6L12,0l7.7,7.7c0.4,0.4,0.4,1,0,1.4L12,16.8z" />
+                </svg>
+              </span>
+              <span class="c-amenity_name">Translation Services</span>
             </div>
           </div>
+
+          <div>
+            Opening hours Monday08:30 - 16:00 Tuesday08:30 - 16:00 Wednesday08:30 - 16:00 Thursday08:30 - 16:00
+            Friday08:30 - 16:00 Saturday09:00 - 12:00 Sunday - closed
+          </div>
+
+          <ul>
+            Payment methods
+            <li>Credit cards</li>
+            <li>Up to 12 rates</li>
+            <li>CAsh discount</li>
+          </ul>
         </article>
       </div>
     </section>
